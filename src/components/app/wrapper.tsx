f2096066626d2167440b6b6015d6f497926088a8
PJijin <pjijin1@gmail.com>
2024-05-05 14:07:45 +0530

    fix: updated cast link

--- src/components/app/wrapper.tsx
+++ src/components/app/wrapper.tsx
@@ -1,36 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useRef } from "react";
-
 const CastWrapper: React.FC<{ href: string; children: React.ReactNode }> = ({
   href,
   children,
 }) => {
-  const router = useRouter();
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
   return (
-    <div
+    <a
       className="grid cursor-pointer grid-flow-row grid-cols-[repeat(14,minmax(0,1fr))] border-b  bg-opacity-20 px-4 py-5"
-      ref={wrapperRef}
-      onClick={(e) => {
-        if (!wrapperRef.current) return;
-
-        const allIgnoredElements = Array.from(
-          wrapperRef.current.querySelectorAll(
-            "a, button, li, img, video, .metadata",
-          ),
-        );
-        for (const el of allIgnoredElements) {
-          if (el.contains(e.target as Node)) return;
-        }
-
-        router.push(href);
-      }}
+      target="_blank"
+      href={href}
     >
       {children}
-    </div>
+    </a>
   );
 };
 
